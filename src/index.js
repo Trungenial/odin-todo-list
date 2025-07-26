8df64b228899d4550c985a2b2ec979ee0cda7ffa
Trungenial <transytrung0752.work@gmail.com>
2025-07-26 19:12:58 +0700

    done initial component on the interface

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import logo from "./icons/task.svg";
+import menuIcon from "./icons/menu.svg";
+import taskHomeIcon from "./icons/task_home.svg";
+import "./styles/style.css";
+
+document.getElementById("menu-icon").src = menuIcon;
+document.getElementById("task-icon").src = taskHomeIcon;
