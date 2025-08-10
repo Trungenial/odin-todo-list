715b5259771e902a1d642b1f73191d3cba2ca24d
Trungenial <transytrung0752.work@gmail.com>
2025-08-10 15:53:09 +0700

    feat(index.html): create form

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import "./icon.js";
 import "./styles/style.css";
 
+//Create show/hide navbar mechanism
 const menuToggle = document.getElementById("nav-menu-toggle");
 const sidebar = document.getElementById("sidebar-container");
-const mainBoard = document.getElementById("main-board");
 
 menuToggle.addEventListener("click", () => {
+    //switch width of the sidabar
     sidebar.classList.toggle("expanded");
 
+    // update aria attributes
     const isExpanded = menuToggle.getAttribute("aria-expanded") === true;
     menuToggle.setAttribute("aria-expanded", !isExpanded);
 });
